927149752d86cfd564558780d3ca0e0d9865de8e
aabccd021 <aabccd021@gmail.com>
2025-05-12 07:10:53 +0700

    Remove redundant substituters and keys from flake.nix.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,6 @@
 {
 
   nixConfig.allow-import-from-derivation = false;
-  nixConfig.extra-substituters = [
-    "https://cache.garnix.io"
-    "https://nix-community.cachix.org"
-  ];
-  nixConfig.extra-trusted-public-keys = [
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  ];
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
