0f80410068d5d847b36a525e3f62ebeb88d8a191
aabccd021 <aabccd021@gmail.com>
2025-07-22 13:21:24 +0700

    chore(deps): Update bun and react types versions

--- bun.nix
+++ bun.nix
@@ -2,9 +2,9 @@
 {
   "@types/bun" = {
     out_path = "@types/bun";
-    name = "@types/bun@1.2.11";
-    url = "https://registry.npmjs.org/@types/bun/-/bun-1.2.11.tgz";
-    hash = "sha512-ZLbbI91EmmGwlWTRWuV6J19IUiUC5YQ3TCEuSHI3usIP75kuoA8/0PVF+LTrbEnVc8JIhpElWOxv1ocI1fJBbw==";
+    name = "@types/bun@1.2.19";
+    url = "https://registry.npmjs.org/@types/bun/-/bun-1.2.19.tgz";
+    hash = "sha512-d9ZCmrH3CJ2uYKXQIUuZ/pUnTqIvLDS0SK7pFmbx8ma+ziH/FRMoAq5bYpRG7y+w1gl+HgyNZbtqgMq4W4e2Lg==";
   };
   "@types/node" = {
     out_path = "@types/node";
@@ -12,11 +12,23 @@
     url = "https://registry.npmjs.org/@types/node/-/node-22.14.1.tgz";
     hash = "sha512-u0HuPQwe/dHrItgHHpmw3N2fYCR6x4ivMNbPHRkBVP4CvN+kiRrKHWk3i8tXiO/joPwXLMYvF9TTF0eqgHIuOw==";
   };
+  "@types/react" = {
+    out_path = "@types/react";
+    name = "@types/react@19.1.8";
+    url = "https://registry.npmjs.org/@types/react/-/react-19.1.8.tgz";
+    hash = "sha512-AwAfQ2Wa5bCx9WP8nZL2uMZWod7J7/JSplxbTmBQ5ms6QpqNYm672H0Vu9ZVKVngQ+ii4R/byguVEUZQyeg44g==";
+  };
   "bun-types" = {
     out_path = "bun-types";
-    name = "bun-types@1.2.11";
-    url = "https://registry.npmjs.org/bun-types/-/bun-types-1.2.11.tgz";
-    hash = "sha512-dbkp5Lo8HDrXkLrONm6bk+yiiYQSntvFUzQp0v3pzTAsXk6FtgVMjdQ+lzFNVAmQFUkPQZ3WMZqH5tTo+Dp/IA==";
+    name = "bun-types@1.2.19";
+    url = "https://registry.npmjs.org/bun-types/-/bun-types-1.2.19.tgz";
+    hash = "sha512-uAOTaZSPuYsWIXRpj7o56Let0g/wjihKCkeRqUBhlLVM/Bt+Fj9xTo+LhC1OV1XDaGkz4hNC80et5xgy+9KTHQ==";
+  };
+  "csstype" = {
+    out_path = "csstype";
+    name = "csstype@3.1.3";
+    url = "https://registry.npmjs.org/csstype/-/csstype-3.1.3.tgz";
+    hash = "sha512-M1uQkMl8rQK/szD0LNhtqxIPLpimGm8sOBwU7lLnCpSbTyY3yeU1Vc7l4KT5zT4s/yOxHH5O7tIuuLOCnLADRw==";
   };
   "undici-types" = {
     out_path = "undici-types";
