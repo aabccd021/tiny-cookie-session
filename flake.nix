72f2d7fdd3fb4af5e4a9a9d016574e7bb81d01a0
aabccd021 <aabccd021@gmail.com>
2025-07-24 15:48:38 +0700

    Remove deploy.yml workflows, update version to 1.0.19 and add auth token check in flake.nix.

--- flake.nix
+++ flake.nix
@@ -87,6 +87,12 @@
         ];
         text = ''
           repo_root=$(git rev-parse --show-toplevel)
+
+          export NPM_CONFIG_USERCONFIG="$repo_root/.npmrc"
+          if ! grep -q "_authToken" "$NPM_CONFIG_USERCONFIG"; then
+            bunx npm login
+          fi
+
           current_version=$(jq -r .version "$repo_root/package.json")
           name=$(jq -r .name "$repo_root/package.json")
           published_version=$(curl -s "https://registry.npmjs.org/$name" | jq -r '.["dist-tags"].latest')
