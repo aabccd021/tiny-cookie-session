f06b880b4cad8e2a044b5376de065be31570e0e8
aabccd021 <aabccd021@gmail.com>
2025-09-06 16:02:35 +0700

    feat(index.test.ts): Add tests for consume state transitions

--- index.test.ts
+++ index.test.ts
@@ -611,6 +611,45 @@ test("consume: state Forked after [10m, attacker, 10m, user, attacker, user]", a
   }
 });
 
+test("consume: state Forked after [10m, attacker, user, attacker, user]", async () => {
+  let userCookie: string | undefined;
+  let attackerCookie: string | undefined;
+  let date: string;
+  const db = dbInit();
+  const config = { ...testConfig, dateNow: () => new Date(date) };
+
+  {
+    date = "2023-10-01T00:00:00Z";
+    const userSession = await login(db, { config });
+    userCookie = setCookie(userCookie, userSession);
+  }
+  attackerCookie = userCookie;
+  {
+    date = "2023-10-01T00:11:00Z";
+    const attackerSession = await consume(db, attackerCookie, config);
+    expect(attackerSession?.state).toEqual("Active");
+    attackerCookie = setCookie(attackerCookie, attackerSession);
+  }
+  {
+    const userSession = await consume(db, userCookie, config);
+    expect(userSession?.state).toEqual("Active");
+    userCookie = setCookie(userCookie, userSession);
+  }
+  {
+    const attackerSession = await consume(db, attackerCookie, config);
+    expect(attackerSession?.state).toEqual("Active");
+    attackerCookie = setCookie(attackerCookie, attackerSession);
+  }
+  {
+    const userSession = await consume(db, userCookie, config);
+    expect(userSession?.state).toEqual("Forked");
+  }
+  {
+    const attackerSession = await consume(db, attackerCookie, config);
+    expect(attackerSession?.state).toEqual("SessionNotFound");
+  }
+});
+
 test("consume: state Forked after [10m, user, 10m, attacker, user, attacker]", async () => {
   let userCookie: string | undefined;
   let attackerCookie: string | undefined;
